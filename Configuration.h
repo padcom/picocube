965ad000cf2fb3e5ec5b56af5c53fdec81146957
Matthias Hryniszak <mhryn@softserveinc.com>
2020-09-19 20:27:30 +0200

    Updated bed size to match the physical dimensions

--- Configuration.h
+++ Configuration.h
@@ -1139,8 +1139,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 200
-#define Y_BED_SIZE 220
+#define X_BED_SIZE 208
+#define Y_BED_SIZE 210
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
